318d3dd4d9fd1c2b5f8934114950cd3737cc7643
IgorGoro91 <igorysc@gmail.com>
2025-01-15 18:07:45 +0100

    add:finito

--- Pari e Dispari/Pari e Dispari.js
+++ Pari e Dispari/Pari e Dispari.js
@@ -7,23 +7,22 @@ Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 let numeroGiocattore = Math.floor(Math.random() *5) +1
 let numeroPc = Math.floor(Math.random() *5) +1
 
-console.log(numeroGiocattore, numeroPc)
+
 
 function sommaNumeri( numeroGiocattore, numeroPc){
    
     if( numeroGiocattore + numeroPc){
 
         somma = numeroGiocattore + numeroPc
-        console.log(somma )
+        
     }
 
        if( somma % 2 ==0){
 
-        console.log("Somma e pari")
+        console.log(` ${somma} Somma e pari`)
        } else{
-        console.log("Somma e dispasi")
+        console.log(` ${somma} Somma e dispasi`)
        }
 
-
-
 }
+        sommaNumeri( numeroGiocattore , numeroPc)
